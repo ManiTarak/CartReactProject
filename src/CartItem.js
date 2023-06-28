55fb6d876d7b8d7147ed8a05c6a60e491faa5b5c
Mani <manikumarjammula999@gmai.com>
2023-06-28 22:25:53 +0530

    implementing decreasing quantity of cart item event

--- src/CartItem.js
+++ src/CartItem.js
@@ -14,6 +14,15 @@ class CartItem extends React.Component{
             qty:this.state.qty+1,
         });
     }
+    decreaseQuantity=()=>{
+        this.setState((prevState)=>{
+            if(prevState.qty==1)
+            return ;
+            return {
+                qty:prevState.qty-1,
+            }
+        })
+    }
     render(){
         const {price,title,qty}=this.state;
         return(
@@ -27,7 +36,7 @@ class CartItem extends React.Component{
                     <div style={{color:'grey'}}>{qty}</div>
                     <div className='cart-item-actions'>
                     <img alt="increase" className="action-icons" onClick={this.increaseQuantity} src="https://cdn-icons-png.flaticon.com/128/1828/1828925.png"/>
-                    <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/43/43625.png"/>
+                    <img alt="decrease" className="action-icons" onClick={this.decreaseQuantity} src="https://cdn-icons-png.flaticon.com/128/43/43625.png"/>
                     <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"/>
                 </div>
                 </div>
